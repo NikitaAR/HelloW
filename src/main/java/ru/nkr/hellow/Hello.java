cd0859ce0b19607100bb50642308bf7ea92c5712
NA.Rukhlov <ruhlovnikita@gmail.com>
2018-05-17 10:52:14 +0300

    first

---
+++ src/main/java/ru/nkr/hellow/Hello.java
@@ -0,0 +1,10 @@
+package ru.nkr.hellow;
+
+/**
+ * Created by NA.Rukhlov on 17.05.2018.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
